007ff032071b72866a654e5d6c12d3eea51a99b3
Fabricio Dias <fabricio270909@gmail.com>
2024-05-24 23:26:45 -0300

    Usando classes para declarar o triângulo

--- ClassesAttr/Program.cs
+++ ClassesAttr/Program.cs
@@ -6,27 +6,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        double xA, xB, xC, yA, yB, yC;
+        Triangulo x, y;
+
+        x = new Triangulo();
+        y = new Triangulo();
 
         Console.WriteLine("Entre com as medidas do triângulo X: ");
-        xA = double.Parse(s: Console.ReadLine(), CultureInfo.InvariantCulture);
-        xB = double.Parse(s: Console.ReadLine(), CultureInfo.InvariantCulture);
-        xC = double.Parse(s: Console.ReadLine(), CultureInfo.InvariantCulture);
+        x.A = double.Parse(s: Console.ReadLine(), CultureInfo.InvariantCulture);
+        x.B = double.Parse(s: Console.ReadLine(), CultureInfo.InvariantCulture);
+        x.C = double.Parse(s: Console.ReadLine(), CultureInfo.InvariantCulture);
 
         Console.WriteLine("Entre com as medidas do triângulo Y: ");
-        yA = double.Parse(s: Console.ReadLine(), CultureInfo.InvariantCulture);
-        yB = double.Parse(s: Console.ReadLine(), CultureInfo.InvariantCulture);
-        yC = double.Parse(s: Console.ReadLine(), CultureInfo.InvariantCulture);
+        y.A = double.Parse(s: Console.ReadLine(), CultureInfo.InvariantCulture);
+        y.B = double.Parse(s: Console.ReadLine(), CultureInfo.InvariantCulture);
+        y.C = double.Parse(s: Console.ReadLine(), CultureInfo.InvariantCulture);
 
         double xArea, yArea;
         double xP, yP;
 
-        xP = (xA + xB + xC) / 2;
-        xArea = Math.Sqrt(xP * (xP - xA) * (xP - xB) * (xP - xC));
+        xP = (x.A + x.B + x.C) / 2;
+        xArea = Math.Sqrt(xP * (xP - x.A) * (xP - x.B) * (xP - x.C));
         Console.WriteLine($"Área de X = {xArea.ToString("F4")}");
 
-        yP = (yA + yB + yC) / 2;
-        yArea = Math.Sqrt(yP * (yP - yA) * (yP - yB) * (yP - yC));
+        yP = (y.A + y.B + y.C) / 2;
+        yArea = Math.Sqrt(yP * (yP - y.A) * (yP - y.B) * (yP - y.C));
         Console.WriteLine($"Área de X = {yArea.ToString("F4")}");
 
         if (xArea > yArea)
